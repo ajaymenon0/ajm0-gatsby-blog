af51f2213df42197473c4b91bb9b572b2f21df5c
Ajay Menon <ajay@Ajays-MacBook-Pro.local>
2019-03-05 00:14:50 +0530

    Adding GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135582515-1`,
       },
     },
     `gatsby-plugin-feed`,
